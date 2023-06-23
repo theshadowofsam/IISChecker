ea28fc79b283bbaf1c9f94e569e7010462573e4e
Samuel Lee <samuel.lee@accudyn.com>
2023-06-23 10:56:17 -0400

    comments

--- requesttest.py
+++ requesttest.py
@@ -14,27 +14,27 @@ URL = "http://acciis:8080/webapi/errors/"   # The page that is supposed to stop 
 
 
 def main():
-    dt = datetime.datetime
+    dt = datetime.datetime  # datetime object is used to create a timestamp
     while True:
         log = ''
-        r = response()
+        r = response()  # fetches response from the IIS server
         
-        if CODES.get(r, False):
+        if CODES.get(r, False): # checks to see if the returned value from the IIS server was a response code
             log = f'GOOD: {dt.now()}: {r}\n'
         else:
             log = f'ERROR -------: {dt.now()}: {r}\n'
         
-        with open('log.txt', 'a') as fd:
+        with open('log.txt', 'a') as fd:    # opens log.txt and appends the log string
             fd.write(log)
         
-        time.sleep(30)
+        time.sleep(30)  # waits 30 seconds just to keep the amount of requests down
         
 
 
 def response():
     try:
-        r = re.get(URL, timeout=15)
-        rc = r.status_code
+        r = re.get(URL, timeout=15) # fires a GET request to URL
+        rc = r.status_code  #http status code of the response, good scenario is usually 403
     except ReadTimeout as rt:   # THIS is the 'error' when the app crashes.
         return f'ERROR: {type(rt)}: {rt}'
     except Exception as e:  # there are a few other errors that happen on occasion, but I don't think the app crashes when they happen.
